40de11faac25a61c8e0188e4c32f21fe4df35e96
Sekonso <ancaadri9@gmail.com>
2026-09-09 20:16:20 +0700

    Fix: Inertia production issue (typo h3h3)

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
